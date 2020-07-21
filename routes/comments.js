c7b0ccf6e876d50338218756ba72bf2426c5eb96
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-07-21 19:35:40 -0300

    update dummy routes to test if id param is showing

--- routes/comments.js
+++ routes/comments.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => res.send('comments index'));
 router.post('/', (req, res) => res.send('comments create'));
-router.get('/:id', (req, res) => res.send('comment show'));
-router.put('/:id', (req, res) => res.send('comment update'));
-router.delete('/:id', (req, res) => res.send('comment destroy'));
+router.get('/:id', (req, res) => res.send('comment show ' + req.params.id));
+router.put('/:id', (req, res) => res.send('comment update ' + req.params.id));
+router.delete('/:id', (req, res) => res.send('comment destroy ' + req.params.id));
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => res.send('posts index'));
 router.post('/', (req, res) => res.send('posts create'));
-router.get('/:id', (req, res) => res.send('post show'));
-router.put('/:id', (req, res) => res.send('post update'));
-router.delete('/:id', (req, res) => res.send('post destroy'));
+router.get('/:id', (req, res) => res.send('post show ' + req.params.id));
+router.put('/:id', (req, res) => res.send('post update ' + req.params.id));
+router.delete('/:id', (req, res) => res.send('post destroy ' + req.params.id));
 
 module.exports = router;
