73a69d3583f84b098f4e06c1e9d0f6571d8f5854
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-07-24 18:11:41 -0300

    fix missing quotes

--- models/post.js
+++ models/post.js
@@ -6,7 +6,7 @@ const PostSchema = new Schema({
   title: { type: String, required: true },
   timestamp: { type: Date, required: true },
   published: { type: Boolean, required: true },
-  comments: [{ type: Schema.Types.ObjectId, ref: Comment }],
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
   content: { type: String, required: true },
 }) 
 
