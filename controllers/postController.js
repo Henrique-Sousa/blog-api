dc4c429e8d41187434b063a3a02730f26bf6ef58
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-07-24 18:26:42 -0300

    add dummy post to test creation of posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,23 @@
-// post list
-exports.index = (req, res) => res.send('posts index');
+const Post = require('../models/post');
 
-exports.create = (req, res) => res.send('posts create');
+// post list
+//exports.index = (req, res) => res.send('posts index');
+exports.index = (req, res) => {
+  Post.find({}).exec((err, result) => res.send(result));
+}
+
+//exports.create = (req, res) => res.send('posts create');
+exports.create = (req, res) => {
+  const post = new Post({
+    title: 'test1',
+    timestamp: Date.now(),
+    published: false,
+    comments: [],
+    content: 'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.'
+  });
+  post.save();
+  return res.send('post created');
+}
 
 exports.show = (req, res) => res.send('post show ' + req.params.id);
 
