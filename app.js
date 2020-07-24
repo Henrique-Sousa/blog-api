4d6372953cf8181cfca4e484ffba17a31c4f44ed
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-07-24 18:05:19 -0300

    estabilish connection to database

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const posts_router = require('./routes/posts');
 const comments_router = require('./routes/comments');
+
+if (process.env.NODE_ENV != 'production') {
+    require("dotenv").config();
+}
+
+const mongoDB = ((process.env.NODE_ENV == 'production') ? process.env.MONGODB_URI : process.env.MONGODB_DEV_URI);
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connection.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const app = express();
 
