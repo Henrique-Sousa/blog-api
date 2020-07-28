35b66eb4a7339c5ffca2f0d467e8c52bc65e4a95
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-07-27 22:59:52 -0300

    change type of author to string

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-  author: { type: Schema.Types.ObjectId, required: true },
+  author: { type: String, required: true },
   timestamp: { type: Date, required: true },
   content: { type: String, required: true },
 }) 
