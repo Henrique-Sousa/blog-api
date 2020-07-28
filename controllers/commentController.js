0ea606e3dfd016be2ccd113adb2b4e5a5847c0d1
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-07-28 19:18:56 -0300

    implement create comment functionality

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,7 +1,21 @@
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+
 // comments list
 exports.index = (req, res) => res.send('comments index');
 
-exports.create = (req, res) => res.send('comments create');
+exports.create = (req, res) => {
+  const comment = new Comment({
+    author: req.body.author,
+    timestamp: Date.now(),
+    content: req.body.content
+  });
+  comment.save().then(comment => {
+    Post.findByIdAndUpdate(req.params.post_id, {"$push": {comments: comment._id}}, {}, (err, post) => {
+      return res.send(comment);
+    }); 
+  });
+}
 
 exports.show = (req, res) => res.send('comment show ' + req.params.comment_id);
 
